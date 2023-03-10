dce67af1e20ce7b6bcb501afc667521969b2abe4
theleeharrison <leeharrison@me.com>
2023-03-10 21:17:24 +0000

    remove xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,8 +22,8 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 #aws xray
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+#from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 #rollbar
 import os
